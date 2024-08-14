3c123160af38852a0b05a088b904f14a8a4b5a94
L'haiba <amineelhaibavivo@gmail.com>
2024-08-14 14:19:24 +0200

    last push

--- face_taker.py
+++ face_taker.py
@@ -4,22 +4,10 @@ import cv2
 import os
 
 def create_directory(directory: str) -> None:
-    """
-    Create a directory if it doesn't exist.
-
-    Parameters:
-        directory (str): The path of the directory to be created.
-    """
     if not os.path.exists(directory):
         os.makedirs(directory)
 
 def get_face_id(directory: str) -> int:
-    """
-    Get the first identifier available
-
-    Parameters:
-        directory (str): The path of the directory of images.
-    """
     user_ids = []
     for filename in os.listdir(directory):
         number = int(os.path.split(filename)[-1].split("-")[1])
@@ -35,14 +23,6 @@ def get_face_id(directory: str) -> int:
     return max_user_ids
 
 def save_name(face_id: int, face_name: str, filename: str) -> None:
-    """
-    Save name on JSON of names
-
-    Parameters:
-        face_id (int): The identifier of user.
-        face_name (str): The user name.
-        filename (str): The file name where to save the pair identifier - name.
-    """
     names_json = None
     if os.path.exists(filename):
         with open(filename, 'r') as fs:
@@ -59,20 +39,15 @@ if __name__ == '__main__':
     cascade_classifier_filename = 'haarcascade_frontalface_default.xml'
     names_json_filename = 'names.json'
 
-    # Create 'images' directory if it doesn't exist
     create_directory(directory)
     
-    # Load the pre-trained face cascade classifier
     faceCascade = cv2.CascadeClassifier(cascade_classifier_filename)
     
-    # Open a connection to the default camera (camera index 0)
     cam = cv2.VideoCapture(0)
     
-    # Set camera dimensions
     cam.set(3, 640)
     cam.set(4, 480)
     
-    # Initialize face capture variables
     count = 0
     face_name = input('\nEnter user name and press <return> -->  ')
     face_id = get_face_id(directory)
@@ -80,43 +55,30 @@ if __name__ == '__main__':
     print('\n[INFO] Initializing face capture. Look at the camera and wait...')
 
     while True:
-        # Read a frame from the camera
         ret, img = cam.read()
     
-        # Convert the frame to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-        # Detect faces in the frame
         faces = faceCascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
     
-        # Process each detected face
         for (x, y, w, h) in faces:
-            # Draw a rectangle around the detected face
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     
-            # Increment the count for naming the saved images
             count += 1
 
-            # Save the captured image into the 'images' directory
             cv2.imwrite(f'./images/Users-{face_id}-{count}.jpg', gray[y:y+h, x:x+w])
     
-            # Display the image with rectangles around faces
             cv2.imshow('image', img)
     
-        # Press Escape to end the program
         k = cv2.waitKey(100) & 0xff
         if k < 100:
             break
     
-        # Take 30 face samples and stop video. You may increase or decrease the number of
-        # images. The more, the better while training the model.
         elif count >= 100:
             break
     
     print('\n[INFO] Success! Exiting Program.')
     
-    # Release the camera
     cam.release()
     
-    # Close all OpenCV windows
     cv2.destroyAllWindows()
--- get-pip.py
+++ get-pip.py
@@ -1,25 +1,3 @@
-#!/usr/bin/env python
-#
-# Hi There!
-#
-# You may be wondering what this giant blob of binary data here is, you might
-# even be worried that we're up to something nefarious (good for you for being
-# paranoid!). This is a base85 encoding of a zip file, this zip file contains
-# an entire copy of pip (version 24.2).
-#
-# Pip is a thing that installs packages, pip itself is a package that someone
-# might want to install, especially if they're looking to run this get-pip.py
-# script. Pip has a lot of code to deal with the security of installing
-# packages, various edge cases on various platforms, and other such sort of
-# "tribal knowledge" that has been encoded in its code base. Because of this
-# we basically include an entire copy of pip inside this blob. We do this
-# because the alternatives are attempt to implement a "minipip" that probably
-# doesn't do things correctly and has weird edge cases, or compress pip itself
-# down into a single file.
-#
-# If you're wondering how this is created, it is generated using
-# `scripts/generate.py` in https://github.com/pypa/get-pip.
-
 import sys
 
 this_python = sys.version_info[:2]
@@ -44,9 +22,6 @@ from base64 import b85decode
 
 
 def include_setuptools(args):
-    """
-    Install setuptools only if absent and not excluded.
-    """
     cli = not args.no_setuptools
     env = not os.environ.get("PIP_NO_SETUPTOOLS")
     absent = not importlib.util.find_spec("setuptools")
@@ -54,9 +29,6 @@ def include_setuptools(args):
 
 
 def include_wheel(args):
-    """
-    Install wheel only if absent and not excluded.
-    """
     cli = not args.no_wheel
     env = not os.environ.get("PIP_NO_WHEEL")
     absent = not importlib.util.find_spec("wheel")
@@ -81,17 +53,8 @@ def determine_pip_install_arguments():
 
 
 def monkeypatch_for_cert(tmpdir):
-    """Patches `pip install` to provide default certificate with the lowest priority.
-
-    This ensures that the bundled certificates are used unless the user specifies a
-    custom cert via any of pip's option passing mechanisms (config, env-var, CLI).
-
-    A monkeypatch is the easiest way to achieve this, without messing too much with
-    the rest of pip's internals.
-    """
     from pip._internal.commands.install import InstallCommand
 
-    # We want to be using the internal certificates.
     cert_path = os.path.join(tmpdir, "cacert.pem")
     with open(cert_path, "wb") as cert:
         cert.write(pkgutil.get_data("pip._vendor.certifi", "cacert.pem"))
@@ -100,7 +63,6 @@ def monkeypatch_for_cert(tmpdir):
 
     def cert_parse_args(self, args):
         if not self.parser.get_default_values().cert:
-            # There are no user provided cert -- force use of bundled cert
             self.parser.defaults["cert"] = cert_path  # calculated above
         return install_parse_args(self, args)
 
@@ -110,8 +72,6 @@ def monkeypatch_for_cert(tmpdir):
 def bootstrap(tmpdir):
     monkeypatch_for_cert(tmpdir)
 
-    # Execute the included pip and use it to install the latest pip and
-    # setuptools from PyPI
     from pip._internal.cli.main import main as pip_entry_point
     args = determine_pip_install_arguments()
     sys.exit(pip_entry_point(args))
@@ -120,21 +80,16 @@ def bootstrap(tmpdir):
 def main():
     tmpdir = None
     try:
-        # Create a temporary working directory
         tmpdir = tempfile.mkdtemp()
 
-        # Unpack the zipfile into the temporary directory
         pip_zip = os.path.join(tmpdir, "pip.zip")
         with open(pip_zip, "wb") as fp:
             fp.write(b85decode(DATA.replace(b"\n", b"")))
 
-        # Add the zipfile to sys.path so that we can import it
         sys.path.insert(0, pip_zip)
 
-        # Run the bootstrap
         bootstrap(tmpdir=tmpdir)
     finally:
-        # Clean up our temporary working directory
         if tmpdir:
             shutil.rmtree(tmpdir, ignore_errors=True)
 
