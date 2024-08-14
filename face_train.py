29688c124213e55b9ea6b7632a17ff7a552bdcc5
L'haiba <amineelhaibavivo@gmail.com>
2024-08-14 14:20:40 +0200

    last push

--- face_train.py
+++ face_train.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
             id = int(os.path.split(imagePath)[-1].split("-")[1])
     
             faces = detector.detectMultiScale(img_numpy)
-    
+
             for (x, y, w, h) in faces:
                 faceSamples.append(img_numpy[y:y+h, x:x+w])
                 ids.append(id)
